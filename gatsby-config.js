5d897c2692b603a48adb2a10ba40a9f6a085922e
Glenn-Erik Sandbakken <glennerik@gmail.com>
2019-10-08 15:06:33 +0200

    change some base defaults

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MyIP.info`,
+    description: `Shows you your public IP address. Your Internet Protocol adress`,
+    author: `@glenneriks`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
