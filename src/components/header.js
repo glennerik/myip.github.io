b4e2f36d5b535b14e152237ec7df3219fd567796
Glenn-Erik Sandbakken <glennerik@gmail.com>
2019-10-09 16:54:00 +0200

    add more links to top navbar

--- src/components/header.js
+++ src/components/header.js
@@ -14,8 +14,8 @@ const Header = ({ siteTitle }) => (
       aria-label="main navigation"
     >
       <div className="navbar-brand">
-        <Link to="/" className="navbar-item2" title="MyIP.info">
-          <div className="image is-64x64 has-margin-5">
+        <Link to="/" title="MyIP.info">
+          <div className="image is-64x64 has-margin-10">
             <Logo />
           </div>
         </Link>
@@ -25,12 +25,45 @@ const Header = ({ siteTitle }) => (
           className="navbar-burger burger"
           aria-label="menu"
           aria-expanded="false"
-          data-target="navbarBasicExample"
+          data-target="myIpTopNavbarOpen"
         >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
         </a>
+      </div>
+
+      <div id="myIpTopNavbarOpen" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">My IP</a>
+          <a className="navbar-item">My Browser</a>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">More</a>
+            <div className="navbar-dropdown">
+              <a className="navbar-item">About</a>
+              <a className="navbar-item">Privacy</a>
+              <a className="navbar-item">Contact</a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">Report an issue</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+            </div>
+          </div>
+          <div className="navbar-item">
+            <Link to="/">
+              MyIP.info
+              <br />© {new Date().getFullYear()}
+            </Link>
+          </div>
+        </div>
       </div>
     </nav>
   </header>
