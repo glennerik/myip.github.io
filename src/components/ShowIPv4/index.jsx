68c72a18d8d01cceb17459551ce5d0fd5347bfb6
Glenn-Erik Sandbakken <glennerik@gmail.com>
2019-10-09 13:58:40 +0200

    show ur actual IP add reducer and swal

---
+++ src/components/ShowIPv4/index.jsx
@@ -0,0 +1,73 @@
+import React, { useReducer, useEffect } from "react"
+import axios from "axios"
+import reducer, { defaultState } from "./reducer"
+import Swal from "sweetalert2"
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+})
+
+const copyIpv4 = () => {
+  try {
+    const copyText = document.getElementById("copyIpv4")
+    copyText.select()
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy")
+    console.log("copied", copyText.value)
+    if (window.getSelection) {
+      window.getSelection().removeAllRanges()
+    } else if (document.selection) {
+      document.selection.empty()
+    }
+    Toast.fire({
+      type: "success",
+      title: "Copied",
+    })
+  } catch (e) {
+    console.log("Copy failed", e)
+    Toast.fire({
+      type: "error",
+      title: "Copy Failed",
+    })
+  }
+}
+
+export default ({ className }) => {
+  const [state, dispatch] = useReducer(reducer, defaultState)
+
+  useEffect(() => {
+    if (!state.ipv4) {
+      axios
+        .get("https://ipv4.myip.info")
+        .then(({ data: payload }) => {
+          dispatch({ type: "SET_IPV4_VALUE", payload })
+        })
+        .catch(payload => dispatch({ type: "SET_IPV4_FAILED", payload }))
+    }
+  }, [state.ipv4, dispatch])
+
+  if (state.failedMessage) {
+    return <span className={`${className}`}>{state.failedMessage}</span>
+  }
+
+  if (state.isLoading) {
+    return <span className={`${className} button is-loading`}>Loading...</span>
+  }
+
+  return (
+    <>
+      <input
+        id="copyIpv4"
+        value={state.ipv4}
+        readOnly
+        onClick={copyIpv4}
+        className={`${className} has-text-centered has-cursor-pointer is-borderless`}
+        title="Click to copy"
+      />
+      <div className="is-size-7">(click to copy)</div>
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ShowIpv4 from "../components/ShowIPv4/"
+
+// ! make a pageRefresh dropdown + default meta-http-equiv=refresh
 
 const IndexPage = () => (
   <Layout>
     <SEO title="What is my IP address?" />
-
-    <section class="hero is-medium is-light is-bold">
-      <div class="hero-body">
-        <div class="container has-text-centered">
-          <h1 className="title">Your IP address is</h1>
-          <span class="tag is-dark is-large">156.234.13.199</span>
+    <section className="hero is-medium is-light is-bold">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title">Your IP address</h1>
+          <ShowIpv4 className="tag is-dark is-large" />
         </div>
       </div>
     </section>
