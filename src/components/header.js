0d1baf5addae08b0b8bea2cbf4655caa59c5dbff
Glenn-Erik Sandbakken <glennerik@gmail.com>
2019-10-09 12:21:33 +0200

    start make css navbar WIP

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,42 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Logo from "./image"
+
+// make a css navbar
+// https://bulma.io/documentation/components/navbar/
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `steelblue`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+  <header>
+    <nav
+      className="navbar is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+      <div className="navbar-brand">
+        <div
+          className="navbar-item"
+          style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}
         >
+          <Logo />
+        </div>
+
+        <Link to="/" className="navbar-item">
           {siteTitle}
         </Link>
-      </h1>
-    </div>
+
+        <button
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </button>
+      </div>
+    </nav>
   </header>
 )
 
