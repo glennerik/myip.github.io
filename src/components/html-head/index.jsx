3f36d758631db8b5d63c7d80ac97e27f1d2864f6
Glenn-Erik Sandbakken <glennerik@gmail.com>
2019-10-11 16:12:18 +0200

    refactor

---
+++ src/components/html-head/index.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import AddBodyClass from "react-body-classname"
+import Helmet from "react-helmet"
+
+const isProd = process.env.NODE_ENV === "production"
+
+export default () => (
+  <>
+    <AddBodyClass className="has-navbar-fixed-top" />
+    <Helmet>
+      {isProd ? (
+        <script
+          data-ad-client="ca-pub-8326983855300422"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
+      ) : null}
+      <script
+        src="https://kit.fontawesome.com/60966f347e.js"
+        crossorigin="anonymous"
+      />
+    </Helmet>
+  </>
+)
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,15 +8,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import AddBodyClass from "react-body-classname"
-import Helmet from "react-helmet"
 
 import "./layout-bulma.scss"
 import "bulma-helpers/css/bulma-helpers.min.css"
 
+import AddHtmlHead from "./html-head/"
 import Header from "./Header"
-
-const isProd = process.env.NODE_ENV === "production"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -31,20 +28,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Helmet>
-        {isProd ? (
-          <script
-            data-ad-client="ca-pub-8326983855300422"
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          />
-        ) : null}
-        <script
-          src="https://kit.fontawesome.com/60966f347e.js"
-          crossorigin="anonymous"
-        />
-      </Helmet>
-      <AddBodyClass className="has-navbar-fixed-top" />
+      <AddHtmlHead />
       <Header siteTitle={data.site.siteMetadata.title} />
       <main>{children}</main>
     </>
