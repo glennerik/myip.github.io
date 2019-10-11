0877dad8e039eed43a5621b2bc9b1df4c2ae2b14
Glenn-Erik Sandbakken <glennerik@gmail.com>
2019-10-11 13:48:19 +0200

    change 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,8 +7,17 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist...</p>
+    <section className="hero is-fullheight-with-navbar is-light is-bold">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title">NOT FOUND</h1>
+          <p>You just hit a route that doesn&#39;t exist...</p>
+          <p>
+            <Link to="/">Go to the front page</Link>
+          </p>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
